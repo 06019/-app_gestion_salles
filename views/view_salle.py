7f07dd6a6b694ba9c90d3c9beaf72506b964a340
ferhat <ferhat.mehdioui@monboreal.ca>
2026-04-19 15:19:06 -0400

    cadre information implemente

--- views/view_salle.py
+++ views/view_salle.py
@@ -13,3 +13,29 @@ class ViewSalle(ctk.CTk):
         self.title("Application de gestion des salles")
         self.geometry("700x500")
 
+
+        self.cadreInfo = ctk.CTkFrame(self, corner_radius=10)
+        self.cadreInfo.pack(pady=10, padx=10)
+
+        self.lblCode = ctk.CTkLabel(self.cadreInfo, text="Code salle")
+        self.lblCode.grid(row=0, column=0, padx=10, pady=10)
+        self.txtCode = ctk.CTkEntry(self.cadreInfo)
+        self.txtCode.grid(row=0, column=1, padx=10, pady=10)
+
+        self.lblLibelle = ctk.CTkLabel(self.cadreInfo, text="Libellé")
+        self.lblLibelle.grid(row=1, column=0, padx=10, pady=10)
+        self.txtLibelle = ctk.CTkEntry(self.cadreInfo)
+        self.txtLibelle.grid(row=1, column=1, padx=10, pady=10)
+
+        self.lblType = ctk.CTkLabel(self.cadreInfo, text="Type")
+        self.lblType.grid(row=2, column=0, padx=10, pady=10)
+        self.txtType = ctk.CTkEntry(self.cadreInfo)
+        self.txtType.grid(row=2, column=1, padx=10, pady=10)
+
+        self.lblCapacite = ctk.CTkLabel(self.cadreInfo, text="Capacité")
+        self.lblCapacite.grid(row=3, column=0, padx=10, pady=10)
+        self.txtCapacite = ctk.CTkEntry(self.cadreInfo)
+        self.txtCapacite.grid(row=3, column=1, padx=10, pady=10)
+
+
+
