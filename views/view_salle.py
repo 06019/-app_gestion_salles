626f21ead76f138373a61445209eff4301e1b7b9
ferhat <ferhat.mehdioui@monboreal.ca>
2026-04-19 15:06:11 -0400

    creation  de la class ViewSalle

---
+++ views/view_salle.py
@@ -0,0 +1,15 @@
+import customtkinter as ctk
+from tkinter import ttk
+from models.salle import Salle
+from services.service_salle import ServiceSalle
+
+
+class ViewSalle(ctk.CTk):
+    def __init__(self):
+        super().__init__()
+
+        self.service_salle = ServiceSalle()
+
+        self.title("Application de gestion des salles")
+        self.geometry("700x500")
+
