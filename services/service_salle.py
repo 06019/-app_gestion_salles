21fd3da07e041aab498e05df43d0b289eca90a0f
ferhat <ferhat.mehdioui@monboreal.ca>
2026-04-19 13:45:57 -0400

    methode modifier_salle implementee

--- services/service_salle.py
+++ services/service_salle.py
@@ -14,3 +14,12 @@ class ServiceSalle:
             self.dao_salle.insert_salle(salle)
             return True, "Salle ajoutée avec succès"
 
+    def modifier_salle(self, salle):
+        if salle.code == "" or salle.libelle == "" or salle.type == "" or str(salle.capacite) == "":
+            return False, "Tous les champs sont obligatoires"
+
+        if int(salle.capacite) < 1:
+            return False, "La capacité doit être supérieure ou égale à 1"
+
+        self.dao_salle.update_salle(salle)
+        return True, "Salle modifiée avec succès"
