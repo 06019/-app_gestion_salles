060acf45593c015254ae9806a35b42cfdcec5d29
ferhat <ferhat.mehdioui@monboreal.ca>
2026-04-19 16:15:05 -0400

    ajouter_salle implementer

--- views/view_salle.py
+++ views/view_salle.py
@@ -1,5 +1,4 @@
 import customtkinter as ctk
-from tkinter import ttk
 from models.salle import Salle
 from services.service_salle import ServiceSalle
 
@@ -13,7 +12,7 @@ class ViewSalle(ctk.CTk):
         self.title("Application de gestion des salles")
         self.geometry("700x500")
 
-
+        # Cadre Informations Salle
         self.cadreInfo = ctk.CTkFrame(self, corner_radius=10)
         self.cadreInfo.pack(pady=10, padx=10)
 
@@ -37,24 +36,29 @@ class ViewSalle(ctk.CTk):
         self.txtCapacite = ctk.CTkEntry(self.cadreInfo)
         self.txtCapacite.grid(row=3, column=1, padx=10, pady=10)
 
-
+        # Cadre Actions
         self.cadreAction = ctk.CTkFrame(self, corner_radius=10)
         self.cadreAction.pack(pady=10, padx=10)
 
-        self.btnAjouter = ctk.CTkButton(self.cadreAction, text="Ajouter", width=70)
+        self.btnAjouter = ctk.CTkButton(self.cadreAction, text="Ajouter", width=70, command=self.ajouter_salle)
         self.btnAjouter.grid(row=0, column=0, padx=10, pady=10)
 
-        self.btnModifier = ctk.CTkButton(self.cadreAction, text="Supprimer", width=70)
-        self.btnModifier.grid(row=0, column=1, padx=10, pady=10)
+        self.btnSupprimer = ctk.CTkButton(self.cadreAction, text="Supprimer", width=70, command=self.supprimer_salle)
+        self.btnSupprimer.grid(row=0, column=1, padx=10, pady=10)
 
-        self.btnSupprimer = ctk.CTkButton(self.cadreAction, text="Modifier", width=70)
-        self.btnSupprimer.grid(row=0, column=2, padx=10, pady=10)
+        self.btnModifier = ctk.CTkButton(self.cadreAction, text="Modifier", width=70, command=self.modifier_salle)
+        self.btnModifier.grid(row=0, column=2, padx=10, pady=10)
 
-        self.btnRechercher = ctk.CTkButton(self.cadreAction, text="Rechercher",width=70)
+        self.btnRechercher = ctk.CTkButton(self.cadreAction, text="Rechercher", width=70, command=self.rechercher_salle)
         self.btnRechercher.grid(row=0, column=3, padx=10, pady=10)
 
+    def ajouter_salle(self):
+        salle = Salle(
+            self.txtCode.get(),
+            self.txtLibelle.get(),
+            self.txtType.get(),
+            self.txtCapacite.get()
+        )
 
-
-
-
+        self.service_salle.ajouter_salle(salle)
 
